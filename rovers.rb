6215485c02a61553c93a6f9a7e9cfc4c9bb14a04
charlieperson <charlie.person@hyperisland.se>
2016-05-13 03:00:50 +0100

    Working code- about to refactor into methods

--- rovers.rb
+++ rovers.rb
@@ -1,32 +1,54 @@
 puts 'What is the top right most coordinate?'
-upper_right = gets.chomp
+upper_right = gets.chomp.split(' ')
 puts 'Where is the rover currently?'
-starting_location = gets.chomp
-start_x = starting_location.split(' ')[0].to_i
-start_y = starting_location.split(' ')[1].to_i
-start_point = [start_x, start_y]
-direction = starting_location.split(' ')[2]
-puts "Starting x position = " + start_x.to_s
-puts "Starting y position = " + start_y.to_s
+start_point = gets.chomp.split(' ')
+x = start_point[0].to_i
+y = start_point[1].to_i
+location = [x, y]
+direction = start_point[2]
+puts "Starting x position = " + x.to_s
+puts "Starting y position = " + y.to_s
 puts "Starting direction = " + direction
 
 puts 'Where would you like me to go?'
 directions = gets.chomp.split('')
-puts directions
+
+def move
+  location[0] += orientation[direction][0]
+  location[1] += orientation[direction][1]
+end
+
+def turn_left
+  direction = nesw[nesw.index(direction) - 1]
+end
+
+def turn_right
+  direction == 'W' ? direction = 'N' : direction = nesw[nesw.index(direction) + 1]
+end
+
+def fallen?(location, upper_right)
+  abort('You fell') if location[0] < 0 || location[0] > upper_right[0].to_i || location[1] < 0 || location[1] > upper_right[1].to_i
+end
+
 orientation = {'N' => [0, 1], 'E' => [1, 0], 'S' => [0, -1], 'W' => [-1, 0]}
 
 nesw = ['N', 'E', 'S', 'W']
 
 directions.each do |d|
   if d == 'M'
-    start_point[0] += orientation[direction][0]
-    start_point[1] += orientation[direction][1]
-    puts start_point.to_s
+    location[0] += orientation[direction][0]
+    location[1] += orientation[direction][1]
+    puts location.to_s
   elsif d == 'L'
     direction = nesw[nesw.index(direction) - 1]
     puts 'direction: ' + direction
   elsif d == 'R'
-    direction = nesw[nesw.index(direction) + 1]
+    direction == 'W' ? direction = 'N' : direction = nesw[nesw.index(direction) + 1]
     puts 'direction: ' + direction
+  else
+    abort('Not valid directions.')
   end
+  fallen?(location, upper_right)
 end
+
+puts location.to_s + ' ' + direction
