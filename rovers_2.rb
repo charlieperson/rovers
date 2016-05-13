9b3194f328267f5ff97b6fc098bb67eefb30213b
charlieperson <charlie.person@hyperisland.se>
2016-05-13 12:02:21 +0100

    Separates initialize into methods

--- rovers_2.rb
+++ rovers_2.rb
@@ -7,12 +7,24 @@ class Rover
   $nesw = ['N', 'E', 'S', 'W']
 
   def initialize
+    set_plateau
+    set_location
+    set_directions
+  end
+
+  def set_plateau
     puts 'What is the top right most coordinate?'
     @upper_right = gets.chomp.split(' ')
+  end
+
+  def set_location
     puts 'Where is the rover currently?'
     @start_point = gets.chomp.split(' ')
     @heading = @start_point[2]
     @location = [@start_point[0].to_i, @start_point[1].to_i]
+  end
+
+  def set_directions
     puts 'Where would you like me to go?'
     @directions = gets.chomp.split('')
   end
