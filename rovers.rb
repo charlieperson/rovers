0c7865abcf04a8a238140dbe9316ee3333d920f8
charlieperson <charlie.person@hyperisland.se>
2016-05-13 11:27:53 +0100

    Adds version_2 (extracted into Rover class)

--- rovers.rb
+++ rovers.rb
@@ -5,10 +5,10 @@ start_point = gets.chomp.split(' ')
 x = start_point[0].to_i
 y = start_point[1].to_i
 location = [x, y]
-direction = start_point[2]
+heading = start_point[2]
 puts "Starting x position = " + x.to_s
 puts "Starting y position = " + y.to_s
-puts "Starting direction = " + direction
+puts "Starting heading = " + heading
 
 puts 'Where would you like me to go?'
 directions = gets.chomp.split('')
@@ -16,17 +16,17 @@ directions = gets.chomp.split('')
 orientation = {'N' => [0, 1], 'E' => [1, 0], 'S' => [0, -1], 'W' => [-1, 0]}
 nesw = ['N', 'E', 'S', 'W']
 
-def move(location, orientation, direction)
-  location[0] += orientation[direction][0]
-  location[1] += orientation[direction][1]
+def move(location, orientation, heading)
+  location[0] += orientation[heading][0]
+  location[1] += orientation[heading][1]
 end
 
-def turn_left(direction, nesw)
-  direction = nesw[nesw.index(direction) - 1]
+def turn_left(heading, nesw)
+  heading = nesw[nesw.index(heading) - 1]
 end
 
-def turn_right(direction, nesw)
-  direction == 'W' ? direction = 'N' : direction = nesw[nesw.index(direction) + 1]
+def turn_right(heading, nesw)
+  heading == 'W' ? heading = 'N' : heading = nesw[nesw.index(heading) + 1]
 end
 
 def fallen?(location, upper_right)
@@ -35,19 +35,19 @@ end
 
 directions.each do |d|
   if d == 'M'
-    location[0] += orientation[direction][0]
-    location[1] += orientation[direction][1]
+    location[0] += orientation[heading][0]
+    location[1] += orientation[heading][1]
     puts location.to_s
   elsif d == 'L'
-    direction = nesw[nesw.index(direction) - 1]
-    puts 'direction: ' + direction
+    heading = nesw[nesw.index(heading) - 1]
+    puts 'heading: ' + heading
   elsif d == 'R'
-    direction == 'W' ? direction = 'N' : direction = nesw[nesw.index(direction) + 1]
-    puts 'direction: ' + direction
+    heading == 'W' ? heading = 'N' : heading = nesw[nesw.index(heading) + 1]
+    puts 'heading: ' + heading
   else
     abort('Not valid directions.')
   end
   fallen?(location, upper_right)
 end
 
-puts location.to_s + ' ' + direction
+puts location.join(' ') + ' ' + heading
--- rovers_2.rb
+++ rovers_2.rb
@@ -1,48 +1,44 @@
 class Rover
 
-  attr_reader :bikes, :capacity, :broken_bikes
+  attr_reader :directions, :upper_right, :nesw, :orientation
+  attr_accessor :location, :heading
 
-  def initialize(capcity = DEFAULT_CAPACITY)
+  def initialize
     @orientation = {'N' => [0, 1], 'E' => [1, 0], 'S' => [0, -1], 'W' => [-1, 0]}
     @nesw = ['N', 'E', 'S', 'W']
-
-    @bikes = []
-    @broken_bikes = []
-    @capacity = capcity
+    puts 'What is the top right most coordinate?'
+    @upper_right = gets.chomp.split(' ')
+    puts 'Where is the rover currently?'
+    @start_point = gets.chomp.split(' ')
+    @heading = @start_point[2]
+    @location = [@start_point[0].to_i, @start_point[1].to_i]
+    puts 'Where would you like me to go?'
+    @directions = gets.chomp.split('')
   end
 
-  def release_bike
-    raise("Can't release bike: None available or broken") if @bikes.empty?
-    @bikes.pop
-  end
-
-  def dock(bike)
-    raise("Docking station is full") if dock_is_full?
-    @bikes << bike
-  end
-
-  def dock_broken_bike(bike)
-    raise("Docking station is full") if dock_is_full?
-    bike.report_broken
-    @broken_bikes << bike
-  end
-
-  def collect_broken_bikes
-    give_bikes = broken_bikes.map{|bike| bike}
-    @broken_bikes.clear
-    return give_bikes
-  end
-
-  def receive_working_bikes(bikes)
-    bikes.each do |bike|
-      @bikes << bike
+  def find_my_rover
+    directions.each do |d|
+      if d == 'M'
+        location[0] += orientation[heading][0]
+        location[1] += orientation[heading][1]
+        puts location.to_s
+      elsif d == 'L'
+        heading = nesw[nesw.index(heading) - 1]
+        puts 'heading: ' + heading
+      elsif d == 'R'
+        heading == 'W' ? heading = 'N' : heading = nesw[nesw.index(heading) + 1]
+        puts 'heading: ' + heading
+      else
+        abort('Not valid directions.')
+      end
+      fallen?(location, upper_right)
     end
+    return location.join(' ') + ' ' + heading
   end
 
   private
 
-  def dock_is_full?
-    bikes.length + broken_bikes.length >= DEFAULT_CAPACITY
+  def fallen?(location, upper_right)
+    abort('You fell') if location[0] < 0 || location[0] > upper_right[0].to_i || location[1] < 0 || location[1] > upper_right[1].to_i
   end
-
 end
